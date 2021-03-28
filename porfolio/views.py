5d7da299aa419d2b137cbd85691d047fb2928852
poonam <poonam.nikam@eduvanz.com>
2021-03-29 01:41:37 +0530

    initial data

---
+++ porfolio/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Job
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
+
+def index(request):
+    return HttpResponse("hi ,welcome");
+
+# Create your views here.
+@csrf_exempt
+def job(request):
+    if request.method=="GET":
+        
+        jobs=Job.objects.all()
+       
+        return render(request,"porfolio/index.html",{"jobs":jobs});
+    if request.method=="POST":
+        body_unicode= request.body.decode('utf-8')
+        data = json.loads(body_unicode)
+        company=data['company']
+        description=data['description']
+
+        job=Job(company=company,description=description)
+        job.save()
+        return HttpResponse("Added");
+    if request.method=="DELETE":
+        body_unicode =request.body.decode('utf-8')
+        data=json.loads(body_unicode)
+        company = data['company']
+
+        job = Job.objects.filter(company=company).delete()
+
+        return HttpResponse("delete successfully")
+
+
